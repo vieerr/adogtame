c2f4ac2e485077fedc6a4d9179021e7339f407f7
CarlosHa44 <carlosha176@gmail.com>
2025-02-06 00:06:41 -0500

    Fixing the logic of the filters and making it modular

--- src/components/dogs/DogFilter.jsx
+++ src/components/dogs/DogFilter.jsx
@@ -1,10 +1,56 @@
+'use client'
+import { useState } from "react";
 import PanelCard from "./PanelCard";
 const DogFilter = () => {
+    const [likedDogs, setLikedDogs] = useState([]);
+    const [filter, setFilter] = useState('');
+
+    const handleLikeClick = (dogId) => {
+        setLikedDogs((prevLikedDogs) =>
+            prevLikedDogs.includes(dogId)
+                ? prevLikedDogs.filter((id) => id !== dogId)
+                : [...prevLikedDogs, dogId]
+        );
+    };
+    const data = [
+        { id: 1, name: 'Olivier', image: '/perro1.jpeg', age: 20, sexo: "Macho", esterilizado: true },
+        { id: 2, name: 'Mathew', image: '/perro2.jpg', age: 19, sexo: "Macho", esterilizado: true },
+        { id: 3, name: 'Antonik', image: '/images.jpg', age: 19, sexo: "Hembra", esterilizado: true },
+        { id: 4, name: 'Nimbus', image: '/perro4.jpg', age: 8, sexo: "Macho", esterilizado: false },
+        { id: 5, name: 'Fredo', image: '/perro5.jpeg', age: 4, sexo: 'Hembra', esterilizado: true },
+        { id: 6, name: 'Bella', image: '/perro1.jpeg', age: 3, sexo: 'Hembra', esterilizado: true },
+        { id: 7, name: 'Charlie', image: '/perro1.jpeg', age: 5, sexo: 'Macho', esterilizado: false },
+        { id: 8, name: 'Max', image: '/perro1.jpeg', age: 2, sexo: 'Macho', esterilizado: true },
+        { id: 9, name: 'Luna', image: '/perro1.jpeg', age: 1, sexo: 'Hembra', esterilizado: false },
+        { id: 10, name: 'Rocky', image: '/perro1.jpeg', age: 6, sexo: 'Macho', esterilizado: true },
+        { id: 11, name: 'Lucy', image: '/perro1.jpeg', age: 4, sexo: 'Hembra', esterilizado: true },
+        { id: 12, name: 'Daisy', image: '/perro1.jpeg', age: 7, sexo: 'Hembra', esterilizado: false },
+        { id: 13, name: 'Bailey', image: '/perro1.jpeg', age: 3, sexo: 'Macho', esterilizado: true },
+        { id: 14, name: 'Molly', image: '/perro1.jpeg', age: 5, sexo: 'Hembra', esterilizado: true },
+        { id: 15, name: 'Buddy', image: '/perro1.jpeg', age: 2, sexo: 'Macho', esterilizado: false },
+        { id: 16, name: 'Maggie', image: '/perro1.jpeg', age: 6, sexo: 'Hembra', esterilizado: true },
+        { id: 17, name: 'Oscar', image: '/perro1.jpeg', age: 4, sexo: 'Macho', esterilizado: true },
+        { id: 18, name: 'Chloe', image: '/perro1.jpeg', age: 1, sexo: 'Hembra', esterilizado: false },
+        { id: 19, name: 'Toby', image: '/perro1.jpeg', age: 3, sexo: 'Macho', esterilizado: true },
+        { id: 20, name: 'Coco', image: '/perro1.jpeg', age: 5, sexo: 'Hembra', esterilizado: true }
+        // Agrega más perros aquí según sea necesario
+    ];
+
+    const filteredDogs = data.filter(dog => {
+        if (!filter) return true;
+        return dog.sexo.toLowerCase().includes(filter.toLowerCase());
+    });
+
+
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content h-full ">
-                <PanelCard />
+                <PanelCard 
+                    data={filteredDogs}
+                    likedDogs={likedDogs}
+                    onLikeClick={handleLikeClick}
+                />
             </div>
             <div className="drawer-side">
                 <label
@@ -45,12 +91,13 @@ const DogFilter = () => {
                     </div>
                     <div className="text-center mt-4">
                         <h3 className="uppercase text-md font-extrabold mb-5">Género</h3>
-                        <select defaultValue={""} className="select bg-secondary-content mb-4 select-bordered w-full mt-2">
+                        <select defaultValue={""} className="select bg-secondary-content mb-4 select-bordered w-full mt-2" value={filter} 
+                            onChange={(e) => setFilter(e.target.value)}>
                             <option  value={""}>
                                 Cualquiera
                             </option>
-                            <option>Masculino</option>
-                            <option>Femenino</option>
+                            <option>Macho</option>
+                            <option>Hembra</option>
                         </select>
                     </div>
                 </ul>
--- src/components/dogs/PanelCard.jsx
+++ src/components/dogs/PanelCard.jsx
@@ -1,51 +1,16 @@
 'use client';
-import { useState } from 'react';
 import CardDog from './CardDog';
 
-const PanelCard = () => {
-    const [likedDogs, setLikedDogs] = useState([]);
-
-    const handleLikeClick = (dogId) => {
-        setLikedDogs((prevLikedDogs) =>
-            prevLikedDogs.includes(dogId)
-                ? prevLikedDogs.filter((id) => id !== dogId)
-                : [...prevLikedDogs, dogId]
-        );
-    };
-
-    const dogs = [
-        { id: 1, name: 'Olivier', image: '/perro1.jpeg', age: 20, sexo: "Macho", esterilizado: true },
-        { id: 2, name: 'Mathew', image: '/perro2.jpg', age: 19, sexo: "Macho", esterilizado: true },
-        { id: 3, name: 'Antonik', image: '/images.jpg', age: 19, sexo: "Hembra", esterilizado: true },
-        { id: 4, name: 'Nimbus', image: '/perro4.jpg', age: 8, sexo: "Macho", esterilizado: false },
-        { id: 5, name: 'Fredo', image: '/perro5.jpeg', age: 4, sexo: 'Hembra', esterilizado: true },
-        { id: 6, name: 'Bella', image: '/perro1.jpeg', age: 3, sexo: 'Hembra', esterilizado: true },
-        { id: 7, name: 'Charlie', image: '/perro1.jpeg', age: 5, sexo: 'Macho', esterilizado: false },
-        { id: 8, name: 'Max', image: '/perro1.jpeg', age: 2, sexo: 'Macho', esterilizado: true },
-        { id: 9, name: 'Luna', image: '/perro1.jpeg', age: 1, sexo: 'Hembra', esterilizado: false },
-        { id: 10, name: 'Rocky', image: '/perro1.jpeg', age: 6, sexo: 'Macho', esterilizado: true },
-        { id: 11, name: 'Lucy', image: '/perro1.jpeg', age: 4, sexo: 'Hembra', esterilizado: true },
-        { id: 12, name: 'Daisy', image: '/perro1.jpeg', age: 7, sexo: 'Hembra', esterilizado: false },
-        { id: 13, name: 'Bailey', image: '/perro1.jpeg', age: 3, sexo: 'Macho', esterilizado: true },
-        { id: 14, name: 'Molly', image: '/perro1.jpeg', age: 5, sexo: 'Hembra', esterilizado: true },
-        { id: 15, name: 'Buddy', image: '/perro1.jpeg', age: 2, sexo: 'Macho', esterilizado: false },
-        { id: 16, name: 'Maggie', image: '/perro1.jpeg', age: 6, sexo: 'Hembra', esterilizado: true },
-        { id: 17, name: 'Oscar', image: '/perro1.jpeg', age: 4, sexo: 'Macho', esterilizado: true },
-        { id: 18, name: 'Chloe', image: '/perro1.jpeg', age: 1, sexo: 'Hembra', esterilizado: false },
-        { id: 19, name: 'Toby', image: '/perro1.jpeg', age: 3, sexo: 'Macho', esterilizado: true },
-        { id: 20, name: 'Coco', image: '/perro1.jpeg', age: 5, sexo: 'Hembra', esterilizado: true }
-        // Agrega más perros aquí según sea necesario
-    ];
-
+const PanelCard = ({data,likedDogs,onLikeClick}) => {
     return (
         <div className="flex justify-center p-4">
            <div className="grid grid-cols-1 md:grid-cols-4 gap-8 w-max items-start">
-                {dogs.map((dog) => (
+                {data.map((dog) => (
                     <CardDog
                         key={dog.id}
                         dog={dog}
                         isLiked={likedDogs.includes(dog.id)}
-                        onLikeClick={handleLikeClick}
+                        onLikeClick={onLikeClick}
                     />
                 ))}
             </div>
